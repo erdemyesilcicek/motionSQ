742a3753b0d718c85ea803400165b9479ce57da2
Erdem Yeşilçiçek <erdmyslcck9@outlook.com>
2023-08-31 12:25:19 +0300

    updates

--- Assets/Scripts/squareMotion.cs
+++ Assets/Scripts/squareMotion.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
